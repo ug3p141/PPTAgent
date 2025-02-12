a56ba0f262d228869d118182697f97b73c371069
Force1ess <forcelesszheng@foxmail.com>
2025-02-12 05:16:01 +0000

    refine eval; use faster_fid

--- src/experiment/evals.py
+++ src/experiment/evals.py
@@ -1,132 +1,177 @@
 import json
 import os
 import random
-import shutil
-import tempfile
+import re
+import sys
 from collections import defaultdict
+from functools import partial
 from glob import glob
-from typing import Literal
+from typing import List
 
 import func_argparse
-import pytorch_fid.fid_score as fid
 import torch
 from jinja2 import Template
-from pytorch_fid.fid_score import compute_statistics_of_path
+
+if os.path.exists("src/experiment/faster-pytorch-fid"):
+    sys.path.append("src/experiment/faster-pytorch-fid")
+    import fid_score_gpu as fid
+else:
+    import pytorch_fid.fid_score as fid
+
 from rich import print
 from tqdm import tqdm
-from transformers import GPT2LMHeadModel, GPT2TokenizerFast
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 import llms
-from presentation import Picture, Presentation, SlidePage
-from utils import Config, pexists, pjoin
+from presentation import Picture, Presentation
+from utils import Config, pdirname, pexists, pjoin
 
 fid.tqdm = lambda x: x
 judges = [
     (llms.gpt4o, llms.gpt4o, "gpt4o"),
-    (llms.qwen2_5, llms.intern_vl, "qwen+intern"),
     (llms.qwen2_5, llms.qwen_vl, "Qwen"),
     (llms.qwen_vl, llms.qwen_vl, "qwen_vl"),
-    (llms.intern_vl, llms.intern_vl, "intern_vl"),
 ]
+
+PPL_MODEL = "/141nfs/zhenghao2022/PPTAgent/Llama-3-8B"
 DEVICES = torch.cuda.device_count()
 
 
-def get_ppl(slide: SlidePage, model: GPT2LMHeadModel, tokenizer: GPT2TokenizerFast):
+def get_eval(prs_source: str):
+    eval_file = pjoin(pdirname(prs_source), "evals.json")
+    if pexists(eval_file):
+        with open(eval_file, "r") as f:
+            return json.load(f), eval_file
+    return {}, eval_file
+
+
+def get_ppl(inputs: List[str], model: AutoModelForCausalLM, tokenizer: AutoTokenizer):
     ppl = []
-    text = slide.to_text()
-    if len(text) == 0:
-        return ppl
-    tokenized = tokenizer(text, return_tensors="pt").to(model.device)
-    with torch.no_grad():
-        outputs = model(tokenized.input_ids, labels=tokenized.input_ids)
-        loss = outputs.loss
-        perplexity = torch.exp(loss)
-        ppl.append(perplexity.item())
+    for sl_text in inputs:
+        tokenized = tokenizer(sl_text, return_tensors="pt").to(model.device)
+        with torch.no_grad():
+            outputs = model(tokenized.input_ids, labels=tokenized.input_ids)
+            loss = outputs.loss
+            perplexity = torch.exp(loss)
+            ppl.append(perplexity.item())
     return ppl
 
 
-def eval_general(presentations: list[Presentation], evals: dict[str, list[int]]):
-    for prs in presentations:
-        if prs.source_file in evals["pages"]:
+def parse_beamer(tex_file: str):
+    slides = []
+    current = []
+    tex = open(tex_file).read()
+    lines = tex.split("\n")
+
+    for line in lines:
+        line = line.strip()
+        if not line:
             continue
-        evals["pages"][prs.source_file] = len(prs)
-        evals["characters"][prs.source_file] = sum(
-            [len(slide.to_text()) for slide in prs.slides]
-        )
-        evals["figures"][prs.source_file] = sum(
-            [len(list(slide.shape_filter(Picture))) for slide in prs.slides]
-        )
 
-
-def eval_feature(
-    presentations: list[Presentation],
-    evals: dict,
-    setting: str,
-):
-    device = f"cuda:{random.randint(0, DEVICES - 1)}"
-    print("start scoring ppl")
-    model = GPT2LMHeadModel.from_pretrained("gpt2").to(device)
-    tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
-    for prs in tqdm(presentations):
-        try:
-            if prs.source_file in evals["ppl"]:
-                continue
-            if (
-                prs.source_file
-                == "data/culture/pptx/ChemBio-in-the-HUB-public/PPTCrew_wo_SchemaInduction/SSRN-id2933553_Management of Systems Engineering and Technical Assistance of DARPA Research Programs/final.pptx"
-            ):
-                continue
-            ppl = []
-            for slide in prs.slides:
-                ppl.extend(get_ppl(slide, model, tokenizer))
-            if len(ppl) == 0:
-                continue
-            evals["ppl"][prs.source_file] = sum(ppl) / len(ppl)
-        except Exception as e:
-            print(e, "\n", "happended in ", prs.source_file)
-
-    model = fid.InceptionV3([fid.InceptionV3.BLOCK_INDEX_BY_DIM[64]]).to(device)
-    for ppt_folder in tqdm(sorted(glob(f"data/*/pptx/*/"))):
-        if ppt_folder in evals["fid"]:
+        if r"\begin{frame}" in line:
+            current = []
             continue
-        source_folder = pjoin(ppt_folder, "source_slides")
-        m1, s1 = compute_statistics_of_path(source_folder, model, 128, 64, device)
-        try:
-            with tempfile.TemporaryDirectory(prefix="ppteval_fid_") as temp_dir:
-                for result_folder in glob(
-                    pjoin(ppt_folder, f"final_images/{setting}/*")
-                ):
-                    folder_base = os.path.basename(result_folder)
-                    for image_file in os.listdir(result_folder):
-                        image_path = os.path.join(result_folder, image_file)
-                        temp_image_path = os.path.join(
-                            temp_dir, folder_base + "_" + image_file
-                        ).replace(" ", "_")
-                        shutil.copyfile(image_path, temp_image_path)
-                if len(os.listdir(temp_dir)) < 10:
-                    continue
-                m2, s2 = compute_statistics_of_path(temp_dir, model, 32, 64, device)
 
-                evals["fid"][ppt_folder] = fid.calculate_frechet_distance(
-                    m1, s1, m2, s2
+        if r"\end{frame}" in line:
+            if current:
+                slides.append(current)
+            continue
+
+        if r"\frametitle" in line:
+            title = re.search(r"\{(.*?)\}", line)
+            if title:
+                current.append(title.group(1))
+            continue
+
+        if r"\item" in line:
+            item = re.search(r"\\item\s+(.*?)$", line)
+            if item:
+                current.append(item.group(1))
+
+    if r"\titlepage" in tex:
+        title = re.search(r"\\title\{(.*?)\}", tex)
+        author = re.search(r"\\author\{(.*?)\}", tex)
+        if title:
+            slides[0].append(title.group(1))
+            if author:
+                slides[0].append(author.group(1))
+
+    return ["\n".join(s) for s in slides]
+
+
+def eval_general(prs_files: list[str]):
+    tmp_config = Config("/tmp")
+    for prs in tqdm(prs_files, desc="General Scoring"):
+        evals, eval_file = get_eval(prs)
+        if prs.endswith(".pptx"):
+            prs = Presentation.from_file(prs, tmp_config)
+            evals["pages"] = len(prs)
+            evals["characters"] = sum([len(slide.to_text()) for slide in prs.slides])
+            evals["figures"] = sum(
+                [len(list(slide.shape_filter(Picture))) for slide in prs.slides]
+            )
+        else:
+            slide_texts = parse_beamer(prs.replace(".pdf", ".tex"))
+            evals["characters"] = sum([len(s) for s in slide_texts])
+            evals["figures"] = len(
+                re.findall(
+                    r"\!\[.*?\]\(.*?\)", open(prs.replace(".pdf", ".tex")).read()
                 )
+            )
+        json.dump(evals, open(eval_file, "w"))
+
+
+def eval_fid(setting: str):
+    device = f"cuda:{random.randint(0, DEVICES - 1)}"
+    if "faster-pytorch-fid" in sys.path:
+        fid.calculate_frechet_distance = partial(
+            fid.calculate_frechet_distance, device=device
+        )
+    model = fid.InceptionV3([fid.InceptionV3.BLOCK_INDEX_BY_DIM[64]]).to(device)
+    for ppt_folder in tqdm(sorted(glob(f"data/*/pptx/*/")), desc="FID Scoring"):
+        source_folder = pjoin(ppt_folder, "source_slides")
+        m1, s1 = fid.compute_statistics_of_path(source_folder, model, 128, 64, device)
+        for result_folder in glob(pjoin(ppt_folder, f"final_images/{setting}/*")):
+            if len(os.listdir(result_folder)) < 10:
+                continue
+            try:
+                m2, s2 = fid.compute_statistics_of_path(
+                    result_folder, model, 32, 64, device
+                )
+                fid_value = fid.calculate_frechet_distance(m1, s1, m2, s2)
+                evals, eval_file = get_eval(result_folder + "/x")
+                evals["fid"] = fid_value
+                json.dump(evals, open(eval_file, "w"))
+            except Exception as e:
+                print(e, "\n", "happended in ", ppt_folder, "on:", setting)
+
+
+def eval_ppl(
+    prs_files: list[str],
+):
+    temp_config = Config("/tmp")
+    device = f"cuda:{random.randint(0, DEVICES - 1)}"
+    model = AutoModelForCausalLM.from_pretrained(PPL_MODEL, device_map=device)
+    tokenizer = AutoTokenizer.from_pretrained(PPL_MODEL)
+    for prs_source in tqdm(prs_files, desc="PPL Scoring"):
+        evals, eval_file = get_eval(prs_source)
+        if evals.get("ppl", None):
+            continue
+        if prs_source.endswith(".pptx"):
+            prs = Presentation.from_file(prs_source, temp_config)
+            slide_texts = [slide.to_text() for slide in prs.slides]
+        elif prs_source.endswith(".pdf"):
+            slide_texts = parse_beamer(prs_source.replace(".pdf", ".tex"))
+        try:
+            ppl = get_ppl(slide_texts, model, tokenizer)
+            evals["ppl"] = sum(ppl) / len(ppl)
+            json.dump(evals, open(eval_file, "w"))
         except Exception as e:
-            print(e, "\n", "happended in ", ppt_folder, "on:", setting)
+            print(e, "\n", "happended in ", prs_source)
 
 
-def merge_evals(folders: list[str], evals: dict):
-    for folder in folders:
-        sub_eval = json.load(open(pjoin(folder, "evals.json")))
-        for dimension in ["content", "vision", "logic"]:
-            evals[dimension] |= sub_eval[dimension]
-    return evals
-
-
-def slide_score(slide_folder: str):
-    eval_file = pjoin(slide_folder, "evals.json")
-    evals = defaultdict(dict)
-    if pexists(eval_file):
-        evals |= json.load(open(eval_file))
+def slide_score(prs_source: str, slide_folder: str):
+    evals, eval_file = get_eval(prs_source)
     text_scorer = Template(open("prompts/ppteval_content.txt", "r").read())
     vision_scorer = Template(open("prompts/ppteval_style.txt", "r").read())
     style_descriptor = open("prompts/ppteval_describe_style.txt", "r").read()
@@ -153,6 +198,8 @@ def slide_score(slide_folder: str):
             evals["content"][slide_image] = llms.language_model(
                 text_scorer.render(descr=content_descr), return_json=True
             )
+    with open(eval_file, "w") as f:
+        json.dump(evals, f)
 
 
 def pres_score(prs_source: str):
@@ -160,7 +207,7 @@ def pres_score(prs_source: str):
         source, setting, pdf, _ = prs_source.rsplit("/", 3)
         slide_folder = os.path.join(source, "final_images", setting, pdf)
     else:  # baseline
-        slide_folder = os.path.dirname(prs_source)
+        slide_folder = pdirname(prs_source)
     eval_file = pjoin(slide_folder, "evals.json")
     evals = defaultdict(dict)
     if pexists(eval_file):
@@ -194,77 +241,76 @@ def pres_score(prs_source: str):
     json.dump(evals, open(eval_file, "w"), indent=4)
 
 
+def ppteval(prs_files: list[str], slide_folders: list[str]):
+    for prs_file in tqdm(prs_files, desc="PPT Scoring"):
+        pres_score(prs_file)
+    for slide_folder in tqdm(slide_folders, desc="Slide Scoring"):
+        slide_score(slide_folder)
+
+
 # ppt eval
 def eval_experiment(
     setting: str,
     general_eval: bool = False,
-    feature_eval: bool = False,
+    ppl_eval: bool = False,
+    fid_eval: bool = False,
     ppt_eval: bool = False,
 ):
-    assert setting != "*"
-    llms.language_model, llms.vision_model, judge_name = judges[0]
-    print(f"evaluating {setting} under {judge_name}")
-    print(
-        "eval config :",
-        f"general_eval: {general_eval}, feature_eval: {feature_eval}, ppt_eval: {ppt_eval}",
-    )
-    eval_file = f"data/evals/{setting}_{judge_name}.json"
-    eval_stats = defaultdict(dict)
-    if pexists(eval_file):
-        eval_stats |= json.load(open(eval_file))
-    config = Config("/tmp")
     prs_files = glob(f"data/*/pptx/*/{setting}/*/final.pptx")
-    # filename dimension score
-    print("start evaluation")
-    if general_eval or feature_eval:
-        presentations = [Presentation.from_file(i, config) for i in prs_files]
-    if general_eval:
-        eval_general(presentations, eval_stats)
+    slide_folders = glob(f"data/*/pptx/*/final_images/{setting}/*")
 
-    if feature_eval:
-        eval_feature(presentations, eval_stats, setting)
+    if general_eval:
+        eval_general(prs_files)
+
+    if ppl_eval:
+        eval_ppl(prs_files)
+
+    if fid_eval:
+        eval_fid(setting)
 
     if ppt_eval:
-        slide_image_folders = glob(f"data/*/pptx/*/final_images/{setting}/*")
-        for presentation in prs_files:
-            pres_score(presentation)
-        eval_stats = merge_evals(slide_image_folders, eval_stats)
-    json.dump(eval_stats, open(eval_file, "w"), indent=4)
+        ppteval(prs_files, slide_folders)
 
 
-def eval_baseline(
-    setting: str,
-    model: Literal["Qwen2.5", "gpt-4o"],
+def eval_docpres(
     general_eval: bool = False,
-    feature_eval: bool = False,
+    ppl_eval: bool = False,
     ppt_eval: bool = False,
 ):
-    evals = defaultdict(dict)
-    prs_files = glob(f"data/*/pdf/*/{setting}/{model}/final.pptx")
-    slide_folders = [os.path.dirname(i) for i in prs_files]
-
-    if general_eval or feature_eval:
-        config = Config("/tmp")
-        presentations = [Presentation.from_file(i, config) for i in prs_files]
+    prs_files = glob(f"data/*/pdf/*/docpres/*/final.pptx")
+    slide_folders = [pdirname(i) for i in prs_files]
 
     if general_eval:
-        eval_general(presentations, evals)
-    if feature_eval:
-        eval_feature(presentations, evals, setting, fid_eval=False)
+        eval_general(prs_files)
+    if ppl_eval:
+        eval_ppl(prs_files)
     if ppt_eval:
-        for slide_folder in slide_folders:
-            slide_score(slide_folder)
-        for presentation in prs_files:
-            pres_score(presentation)
+        ppteval(prs_files, slide_folders)
 
-    merge_evals(slide_folders, evals)
-    json.dump(evals, open(f"data/evals/{setting}_{model}.json", "w"), indent=4)
+
+def eval_kctv(
+    general_eval: bool = False,
+    ppl_eval: bool = False,
+    ppt_eval: bool = False,
+):
+    prs_files = glob(f"data/*/pdf/*/kctv/*/final.pdf")
+    slide_folders = [pdirname(i) for i in prs_files]
+
+    if general_eval:
+        eval_general(prs_files)
+
+    if ppl_eval:
+        eval_ppl(prs_files)
+
+    if ppt_eval:
+        ppteval(prs_files, slide_folders)
 
 
 if __name__ == "__main__":
     func_argparse.main(
         eval_experiment,
-        eval_baseline,
+        eval_docpres,
+        eval_kctv,
         pres_score,
         slide_score,
     )
